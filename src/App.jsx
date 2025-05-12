66824a9874714c07f5a39d787a71d715cd034817
kottor <gardien243@gmail.com>
2025-05-12 13:49:18 +0200

    finito plus que 2 bonus

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,19 @@ import Bar from './components/bar'
 import Btn from './components/btn'
 function App() {
   const [pv,setPv] = useState(100)
+  let random = Math.round(Math.random()*20)
+  let degat = 10
   function restart() {
     setPv(100)
   }
+  pv < 0 && setPv(0);
   function taper(){
-    setPv(pv-10)
+    setPv(pv-degat)
+    random = Math.round(Math.random()*20)
+  }
+  function taper2() {
+    setPv(pv-(degat*2))
+    random = Math.round(Math.random()*20)
   }
   console.log(pv);
   
@@ -20,10 +28,14 @@ function App() {
           <Bar pdv={pv}/>
         </div>
         <div>
-          {pv != 0 ? <Sac pdv={pv} action={taper}/> : <SacCasser pdv={pv}/>}
+          {pv <= 0 ?  <SacCasser pdv={pv}/> : random>6 ?<Sac pdv={pv} action={taper}/>: <Sac pdv={pv} action={taper2}/>}
         </div>
         <div>
-          {pv<=0 ? <Btn valeur='Restart' action={restart}/> :<Btn valeur='Punsh' action={taper}/>}
+          {pv<=0 ? 
+          <Btn valeur='Restart' action={restart}/> 
+          : random<6 ?
+          <Btn valeur='Supa Punsh' action={taper2}/>
+          :<Btn valeur='Punsh' action={taper}/>}
         </div>
       </div>   
     </>
